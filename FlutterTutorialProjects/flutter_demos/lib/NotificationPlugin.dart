f8198fb1dad6332a1c2983f1a3a04462ff4a2c11
Vipin <vipinvijayan23@gmail.com>
2020-06-27 15:53:53 -0400

    Code Update

--- FlutterTutorialProjects/flutter_demos/lib/NotificationPlugin.dart
+++ FlutterTutorialProjects/flutter_demos/lib/NotificationPlugin.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'dart:io' show File, Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
